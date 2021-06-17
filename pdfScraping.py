0ef0aeb4d4f9471dbfd3f88316afb5cf86591f8e
Glauber de Arruda Braga <gab@ic.ufal.br>
2021-06-16 22:08:29 -0300

    Add files via upload

--- pdfScraping.py
+++ pdfScraping.py
@@ -1,5 +1,6 @@
+from os import name
 import camelot
 
 tables = camelot.read_pdf('TISS.pdf', pages='79, 80, 81, 82, 83, 84, 85')
-tables.export('TISS.csv', f='csv')
+tables.export('Teste_Intuitive_Care_Glauber', f='csv', compress=True)
 tables.export
